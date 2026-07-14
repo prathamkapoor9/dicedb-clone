fc5c7635e1e13f8e989ad80d41a2409a2acade10
prathamkapoor <prathamkapoor1357@gmail.com>
2026-07-15 00:12:23 +0530

    use kqueue instead of epoll to run on macOS

--- server/async_tcp.go
+++ server/async_tcp.go
@@ -16,11 +16,11 @@ func RunAsyncTCPServer() error {
 
 	max_clients := 20000
 
-	// Create EPOLL Event Objects to hold events
-	var events []syscall.EpollEvent = make([]syscall.EpollEvent, max_clients)
+	// Create KQUEUE Event Objects to hold events
+	var events []syscall.Kevent_t = make([]syscall.Kevent_t, max_clients)
 
 	// Create a socket
-	serverFD, err := syscall.Socket(syscall.AF_INET, syscall.O_NONBLOCK|syscall.SOCK_STREAM, 0)
+	serverFD, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return err
 	}
@@ -32,7 +32,7 @@ func RunAsyncTCPServer() error {
 	}
 
 	// Bind the IP and the port
-	ip4 := net.ParseIP(config.Host)
+	ip4 := net.ParseIP(config.Host).To4()
 	if err = syscall.Bind(serverFD, &syscall.SockaddrInet4{
 		Port: config.Port,
 		Addr: [4]byte{ip4[0], ip4[1], ip4[2], ip4[3]},
@@ -47,35 +47,36 @@ func RunAsyncTCPServer() error {
 
 	// AsyncIO starts here!!
 
-	// creating EPOLL instance
-	epollFD, err := syscall.EpollCreate1(0)
+	// creating KQUEUE instance
+	kqueueFD, err := syscall.Kqueue()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer syscall.Close(epollFD)
+	defer syscall.Close(kqueueFD)
 
 	// Specify the events we want to get hints about
 	// and set the socket on which
-	var socketServerEvent syscall.EpollEvent = syscall.EpollEvent{
-		Events: syscall.EPOLLIN,
-		Fd:     int32(serverFD),
+	var socketServerEvent syscall.Kevent_t = syscall.Kevent_t{
+		Ident:  uint64(serverFD),
+		Filter: syscall.EVFILT_READ,
+		Flags:  syscall.EV_ADD,
 	}
 
 	// Listen to read events on the Server itself
-	if err = syscall.EpollCtl(epollFD, syscall.EPOLL_CTL_ADD, serverFD, &socketServerEvent); err != nil {
+	if _, err = syscall.Kevent(kqueueFD, []syscall.Kevent_t{socketServerEvent}, nil, nil); err != nil {
 		return err
 	}
 
 	for {
 		// see if any FD is ready for an IO
-		nevents, e := syscall.EpollWait(epollFD, events[:], -1)
+		nevents, e := syscall.Kevent(kqueueFD, nil, events[:], nil)
 		if e != nil {
 			continue
 		}
 
 		for i := 0; i < nevents; i++ {
 			// if the socket server itself is ready for an IO
-			if int(events[i].Fd) == serverFD {
+			if int(events[i].Ident) == serverFD {
 				// accept the incoming connection from a client
 				fd, _, err := syscall.Accept(serverFD)
 				if err != nil {
@@ -85,22 +86,25 @@ func RunAsyncTCPServer() error {
 
 				// increase the number of concurrent clients count
 				con_clients++
-				syscall.SetNonblock(serverFD, true)
+				log.Println("client connected, concurrent clients:", con_clients)
+				syscall.SetNonblock(fd, true)
 
 				// add this new TCP connection to be monitored
-				var socketClientEvent syscall.EpollEvent = syscall.EpollEvent{
-					Events: syscall.EPOLLIN,
-					Fd:     int32(fd),
+				var socketClientEvent syscall.Kevent_t = syscall.Kevent_t{
+					Ident:  uint64(fd),
+					Filter: syscall.EVFILT_READ,
+					Flags:  syscall.EV_ADD,
 				}
-				if err := syscall.EpollCtl(epollFD, syscall.EPOLL_CTL_ADD, fd, &socketClientEvent); err != nil {
+				if _, err := syscall.Kevent(kqueueFD, []syscall.Kevent_t{socketClientEvent}, nil, nil); err != nil {
 					log.Fatal(err)
 				}
 			} else {
-				comm := core.FDComm{Fd: int(events[i].Fd)}
+				comm := core.FDComm{Fd: int(events[i].Ident)}
 				cmd, err := readCommand(comm)
 				if err != nil {
-					syscall.Close(int(events[i].Fd))
+					syscall.Close(int(events[i].Ident))
 					con_clients -= 1
+					log.Println("client disconnected, concurrent clients:", con_clients)
 					continue
 				}
 				respond(cmd, comm)
